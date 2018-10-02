355ed715ac675194587fea8c79a5e5e6fb36062c
ya3rob <buhamad@gmail.com>
2018-10-02 16:37:55 +0300

    lesson done

--- test/test/Controller/LeagueVC.swift
+++ test/test/Controller/LeagueVC.swift
@@ -9,14 +9,31 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    var player: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectNext(league: "Coed")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectNext(league: "Womens")
+    }
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectNext(league: "Mens")
+    }
+    
+    func selectNext(league: String) {
+        player.desiredLeague = league
+        nextButton.isEnabled = true
+    }
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        player = Player()
         // Do any additional setup after loading the view.
     }
     
---
+++ test/test/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  test
+//
+//  Created by yousef buhamad on 10/2/18.
+//  Copyright © 2018 yousef buhamad. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var skillLevel: String?
+}
